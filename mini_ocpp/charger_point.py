d6f0cf4ce54909e52204ca40b58f6889be309206
Nedas Jarmolenka <nedasman@gmail.com>
2024-07-15 01:26:18 +0300

    charger_point: process CALL and CALL_RESULT messages in different functions

--- mini_ocpp/charger_point.py
+++ mini_ocpp/charger_point.py
@@ -27,13 +27,13 @@ class ChargingPoint:
 
     def process_heartbeat(self, response):
         print("Process heartbeat")
+        # TODO: update local clock
 
     async def send_boot_notification(self, websocket):
         message_id = str(uuid.uuid4())
         action = "BootNotification"
         payload = self.form_boot_notification_payload()
         request = [MessageType.CALL.value, message_id, action, payload]
-
         request_json = json.dumps(request)
 
         await websocket.send(request_json)
@@ -60,62 +60,81 @@ class ChargingPoint:
         print(f"Processing ChangeConfiguration with payload: {payload}")
         key = payload["key"]
         value = payload["value"]
-        self.config[key] = value
-        print(f"Changed configuration: {key} = {value}")
-        response_payload = {"status": "Accepted"}
+        if key in self.config:
+            self.config[key] = value
+            print(f"Changed configuration: {key} = {value}")
+            response_payload = {"status": "Accepted"}
+        else:
+            response_payload = {"status": "Rejected"}
+
         response = [MessageType.CALL_RESULT.value, message_id, response_payload]
         return response
 
-    def handle_message(self, websocket, message):
-        print(f"Received: {message}")
-
-        message_type = message[0]
+    def process_call_message(self, message):
         message_id = message[1]
-        if message_type == MessageType.CALL_RESULT.value:
-            if self.__sent_calls[message_id]:
-                action = self.__sent_calls[message_id]
-                payload = message[2]
-                del self.__sent_calls[message_id]
-            else:
-                print("No call message was sent for this message.")
-        elif message_type == MessageType.CALL.value:
-            action = message[2]
-            payload = message[3]
+        action = message[2]
+        payload = message[3]
 
-        if action == "BootNotification":
-            response = self.process_boot_notification(payload)
-        elif action == "GetConfiguration":
+        if action == "GetConfiguration":
             response = self.process_get_configuration(message_id)
-        elif action == "Heartbeat":
-            response = self.process_heartbeat(payload)
         elif action == "ChangeConfiguration":
             response = self.process_change_configuration(message_id, payload)
         else:
             print(f"Received unsupported action: {action}")
 
+        return json.dumps(response)
+
+    def process_call_result_message(self, message):
+        message_id = message[1]
+        if self.__sent_calls[message_id]:
+            action = self.__sent_calls[message_id]
+            payload = message[2]
+            del self.__sent_calls[message_id]
+        else:
+            print("No call message was sent for this message.")
+            return
+
+        if action == "BootNotification":
+            self.process_boot_notification(payload)
+        elif action == "Heartbeat":
+            self.process_heartbeat(payload)
+
+    def handle_message(self, message):
+        print(f"Received: {message}")
+
+        message_type = message[0]
         if message_type == MessageType.CALL.value:
-            return json.dumps(response)
+            return self.process_call_message(message)
+        elif message_type == MessageType.CALL_RESULT.value:
+            self.process_call_result_message(message)
 
     async def listen_for_messages(self, websocket):
         try:
             while True:
                 message = await websocket.recv()
                 message = json.loads(message)
-                response = self.handle_message(websocket, message)
+                response = self.handle_message(message)
                 if response:
                     await websocket.send(response)
                     print(f"Sent: {response}")
         except websockets.exceptions.ConnectionClosed:
             print("WebSocket connection closed.")
 
+    async def heartbeat_task(self, websocket):
+        while True:
+            await self.send_heartbeat(websocket)
+            total_sleep_time = 0
+            while total_sleep_time < self.config["HeartbeatInterval"]:
+                await asyncio.sleep(1)
+                total_sleep_time += 1
+
     async def run(self):
         async with websockets.connect(self.uri) as websocket:
-            # For listening to incoming messages
-            asyncio.create_task(self.listen_for_messages(websocket))
+            listen_task = asyncio.create_task(self.listen_for_messages(websocket))
+            heartbeat_task = asyncio.create_task(self.heartbeat_task(websocket))
+
             await self.send_boot_notification(websocket)
-            while True:
-                await self.send_heartbeat(websocket)
-                await asyncio.sleep(self.config["HeartbeatInterval"])
+            await asyncio.gather(listen_task, heartbeat_task)
 
 
 if __name__ == "__main__":
